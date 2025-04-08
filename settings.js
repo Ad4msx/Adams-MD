b8b0c1bd510cb59253fd886aa2f8fa5e319d2ba9
Ad4msx <pierreadams092@icloud.com>
2025-04-07 21:02:49 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Ad4ms Bot",
+  botOwner: 'Ad4ms', // Your name
+  ownerNumber: '50934100977', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
